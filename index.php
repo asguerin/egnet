3ba2dae191ef5308c13c717f6bf1d59d3c9d4960
asguerin <44937306+asguerin@users.noreply.github.com>
2018-11-10 23:04:45 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
 <?php
-header('Location: home');
+require "../includes/header.inc.php";
  ?>
+
+
+<main>
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8"/>
+      <title>Home | Everlasting Games</title>
+    </head>
+    <body>
+      <div class="body">
+        <div class="container">
+          <div id="title">
+            <h1 class="title">
+              <img src="../img/everlasting-logo.png" alt="Everlasting Games" class="title">
+            </h1>
+          </div>
+
+          <div id="about">
+            <h3 class="heading">About us</h3>
+            <p class="description">Everlasting Games is a student-run game studio devoted to making high quality and engaging games. Founded in Houston, Texas, we're comprised of a small group of passionate developers, now in Houston and Dallas, who have come together to have fun developing games. We try to make our games free if possible, and prefer to only charge when needed.</p>
+          </div>
+
+          <div id="games">
+            <h3 class="heading">Base Wars 3</h3>
+            <p class="description">Base Wars 3 aims to capture the strategic depth and nuance of typical RTS games such as Starcraft while subverting the high skill floor of such games through a turn-based approach.</p>
+            <p class="description">Speedy inputs don't matter as long as you get everything done in your turn, removing the constant reaction-stress and APM of RTS games. However, each turn has a timer on it- you still need to think on your feet and prioritize!</p>
+            <p class="description">This game is capable of both local PvP and Direct Connect PvP. Join the Discord and find a match!</p>
+            <p class="description"><a target="_blank" href="https://discord.gg/Tc4WGbb" class="community">Discord server</a></p>
+            <p class="description"><a target="_blank" href="https://andy98725.itch.io/base-wars" class="download">Download</a></p>
+          </div>
+
+          <div id="contact">
+            <h3 class="heading">Contact us</h3>
+            <p class="description">Andy Hudson: <a href="#" class="mailto">ahudson@everlastinggames.net</a></p>
+            <p class="description">Adrien Guerin: <a href="#" class="mailto">aguerin@everlastinggames.net</a></p>
+          </div>
+
+        </div>
+      </div>
+    </body>
+  </html>
+</main>
+
+<?php
+require "../includes/footer.inc.php";
+?>
